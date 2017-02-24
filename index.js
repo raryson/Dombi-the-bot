8b3366e2d3aca95c51ba19f24d64708a261b804c
Raryson Pereira Rost <raryson.rost@gmail.com>
2017-02-24 17:05:33 -0300

    FIrst commit

---
+++ index.js
@@ -0,0 +1,31 @@
+var express = require('express')
+var app = express()
+const path = require('path')
+const publicPath = path.join(__dirname, 'public')
+const bootstrapPath = path.join(__dirname, 'bower_components/bootstrap/dist')
+const jqueryPath = path.join(__dirname, 'bower_components/jquery/dist')
+var port = 3700
+
+app.use(express.static(publicPath))
+app.use(express.static(bootstrapPath))
+app.use(express.static(jqueryPath))
+
+app.set('view engine', 'ejs')
+
+
+app.get('/', function (req, res) {
+  res.render('page')
+})
+
+
+
+var io = require('socket.io').listen(app.listen(port))
+
+io.sockets.on('connection', function (socket) {
+  socket.emit('message', { message: 'welcome to the chat, tell me you name :)' })
+  socket.on('send', function (data) {
+    io.sockets.emit('message', data)
+  })
+})
+console.log('Listening on port ' + port)
+
---
+++ public/chat.js
@@ -0,0 +1,37 @@
+window.onload = function () {
+  var messages = []
+  var socket = io.connect('http://localhost:3700')
+  var field = document.getElementById('field')
+  var sendButton = document.getElementById('send')
+  var content = document.getElementById('content')
+  var sendName = document.getElementById('sendName')
+  var userName = document.getElementById('name')
+  var messageThings = document.getElementById('sendMessageDiv')
+  var controlThings = document.getElementById('nameDiv')
+  var userNameValue; 
+
+  socket.on('message', function (data) {
+    if (data.message) {
+      messages.push(data.message)
+      var html = ''
+      console.log(userNameValue)
+      for (var i = 0; i < messages.length; i++) {
+        html += messages[i] + '<br />'
+      }
+      content.innerHTML = html
+    } else {
+      console.log('There is a problem:', data)
+    }
+  })
+
+  sendButton.onclick = function () {  
+    var text = userNameValue + ':  ' + field.value
+    socket.emit('send', { message: text })
+  }
+
+  sendName.addEventListener('click', function () {
+    messageThings.style.display = 'block'
+    userNameValue = userName.value
+    controlThings.style.display = 'none'
+  })
+}
